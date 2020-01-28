e53aba9f9ef67a411fa99143200037456cb8c9d7
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2020-01-27 21:54:09 -0500

    using docker, postgres, mutations

--- goBarber/server/src/routes.js
+++ goBarber/server/src/routes.js
@@ -1,4 +1,19 @@
 import { Router } from "express";
+import User from "./app/models/User";
 const routes = new Router();
+
+routes.get("/", async (req, res) => {
+  try {
+    const user = await User.create({
+      name: "Fabricio",
+      email: "fabricio@gmail.com",
+      password_hash: "1234",
+      provider: true
+    });
+    return res.json(user);
+  } catch (error) {
+    throw new Error(error);
+  }
+});
 
 export default routes;
