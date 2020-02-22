a25a0c352f66e3b9933a510d789b4ae5db75d033
Fabricio Policarpo <fabriciopolicarpo0@gmail.com>
2020-02-21 23:03:09 -0500

    changes

--- react-module1/module05/src/pages/Main/index.js
+++ react-module1/module05/src/pages/Main/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
-import { Title } from './styles';
+import React, { useState, useEffect } from 'react'
 
+import { Container, Form, SubmitButton } from './styles'
 
+import { FaGithubAlt, FaPlus } from 'react-icons/fa'
 const Main = () => {
+  const [newRepos, setNewRepos] = useState('')
+
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log(newRepos)
+  }
   return (
-    <Title>
-      Main
-      <small>
-        menor
-      </small>
-    </Title>
+    <Container>
+      <h1>
+        <FaGithubAlt />
+        Repos
+      </h1>
+      <Form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Add repo"
+          onChange={e => setNewRepos(e.target.value)}
+        />
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   )
 }
 
-export default Main;
+export default Main
